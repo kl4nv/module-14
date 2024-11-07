0f4f3d0a4ef094f8f601eb8e645558b5e965d036
kl4nv <kupull94@bk.ru>
2024-11-08 09:21:20 +1000

    Update 14_2.py

--- 14_2.py
+++ 14_2.py
@@ -1,25 +1,27 @@
 import sqlite3
 
 # Связываемся с БД выполненной из прошлого ДЗ
-cursor = sqlite3.connect('not_telegram.db').cursor()
+connection = sqlite3.connect('not_telegram.db')
+cursor = connection.cursor()
 
-#1
+# 1
 cursor.execute('DELETE FROM Users WHERE id = 6')
 
-#2
+# 2
 cursor.execute('SELECT COUNT (*) FROM Users')
-all_count = cursor.fetchone()[0]
+total_count = cursor.fetchone()[0]
 
-#3
+# 3
 cursor.execute('SELECT SUM(balance) FROM Users')
 all_balances = cursor.fetchone()[0]
 
-#4
-print(all_balances/all_count)
+# 4
+print(all_balances/total_count)
 
 # Альтернативный способ найти средний баланс
 cursor.execute('SELECT AVG(balance) FROM Users')
 avg_balance = cursor.fetchone()[0]
 print(avg_balance)
 
-cursor.close()
+connection.commit()
+connection.close()
